5f0b66fc8be4bcb73edc3ec6f044ded6c3053d07
Büşra GÖKTAŞ <goktas.busra@std.izu.edu.tr>
2021-04-11 23:41:12 +0300

    Android Project Homework Tracking App

--- app/src/main/java/com/example/homeworkapplication/model/student.java
+++ app/src/main/java/com/example/homeworkapplication/model/student.java
@@ -1,5 +1,38 @@
 package com.example.homeworkapplication.model;
 
 public class student {
+String Name;
+String Surname;
+int image;
 
+    public student(String name, String surname, int image) {
+        Name = name;
+        Surname = surname;
+        this.image = image;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getSurname() {
+        return Surname;
+    }
+
+    public void setSurname(String surname) {
+        Surname = surname;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
 }
+
